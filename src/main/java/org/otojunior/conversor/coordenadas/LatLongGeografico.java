135e4685d5d8b511869b1d8f0f15a877d3904ed7
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2016-12-15 14:05:34 -0200

    Evolução da refatoração

--- src/main/java/org/otojunior/conversor/coordenadas/LatLongGeografico.java
+++ src/main/java/org/otojunior/conversor/coordenadas/LatLongGeografico.java
@@ -3,9 +3,6 @@
  */
 package org.otojunior.conversor.coordenadas;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
-
 /**
  * @author 01456231650
  *
@@ -16,72 +13,11 @@ public class LatLongGeografico {
 	private ComponenteGeografico longitude;
 	
 	private LatLongGeografico(ComponenteGeografico latitude, ComponenteGeografico longitude) {
-		// TODO Auto-generated constructor stub
+		this.latitude = latitude;
+		this.longitude = longitude;
 	}
 	
-	
-	/**
-	 * 
-	 * @author 01456231650
-	 *
-	 */
-	private static class ComponenteGeografico {
-		private int graus;
-		private int minutos;
-		private double segundos;
-		private char hemisferio;
-		
-		/**
-		 * 
-		 * @param graus
-		 * @param minutos
-		 * @param segundos
-		 * @param hemisferio
-		 */
-		private ComponenteGeografico(int graus, int minutos, double segundos, char hemisferio) {
-			this.graus = graus;
-			this.minutos = minutos;
-			this.segundos = segundos;
-		}
-		
-		/**
-		 * 
-		 * @param graus
-		 * @param minutos
-		 * @param segundos
-		 * @param hemisferio
-		 * @return
-		 */
-		public static ComponenteGeografico valueOf(int graus, int minutos, double segundos, char hemisferio) {
-			return new ComponenteGeografico(graus, minutos, segundos, hemisferio);
-		}
-
-		/**
-		 * @return the graus
-		 */
-		public int getGraus() {
-			return graus;
-		}
-
-		/**
-		 * @return the minutos
-		 */
-		public int getMinutos() {
-			return minutos;
-		}
-
-		/**
-		 * @return the segundos
-		 */
-		public double getSegundos() {
-			return segundos;
-		}
-
-		/**
-		 * @return the hemisferio
-		 */
-		public char getHemisferio() {
-			return hemisferio;
-		}
+	public static LatLongGeografico valueOf(ComponenteGeografico latitude, ComponenteGeografico longitude) {
+		return new LatLongGeografico(latitude, longitude);
 	}
 }
